3290168cd790cc4ec63c7339605eab3c018afc2a
mrs_misteriya <nata.nedostupnaya@mail.com>
2024-06-02 08:40:25 +0300

    change the loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,24 +1,35 @@
+// import React from "react";
 import css from "./Loader.module.css";
-import { RotatingLines } from "react-loader-spinner";
+// import { RotatingLines } from "react-loader-spinner";
+
+// interface Props {
+//   isLoading?: boolean;
+// }
 
 
-export default function Loader() {
-  return (
-    <div className={css.loader}>
-    <RotatingLines
-    visible={true}
-    height="96"
-    width="96"
-    color="#1B5299"
-    strokeWidth="5"
-    animationDuration="0.75"
-    ariaLabel="rotating-lines-loading"
-    wrapperStyle={{}}
-    wrapperClass=""
-    /></div>
-    )
-  
-}
+// const Loader: React.FC<Props> =({isLoading=true})=> {
+//   return (
+//       <div className={css.loader}>
+//         <RotatingLines
+//         visible={isLoading}
+//         height="60"
+//         width="60"
+//         color="#1B5299"
+//         strokeWidth="5"
+//         animationDuration="0.75"
+//         ariaLabel="rotating-lines-loading"
+//         wrapperStyle={{}}
+//         wrapperClass=""
+//         />
+//       </div>
+//     )
+// }
 
+// export default Loader;
 
+export default function () {
+  return (<>
+    <b>Loading...</b>
+  </>)
+ }
 
